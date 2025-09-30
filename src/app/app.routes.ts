d38467e9345266df679e8c1240a370b236e1fd43
Coletti <matheus.coletti08@gmail.com>
2025-09-29 23:26:51 -0300

    feat: new routes/default routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
+import { Home } from './routes/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",
+     component: Home
+    }
+];
