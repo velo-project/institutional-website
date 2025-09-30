b947fff8d306313281f9ef740d73c4a01340ad8f
Coletti <matheus.coletti08@gmail.com>
2025-09-29 23:26:05 -0300

    feat: use router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
+  providers: [provideAnimations()]
 })
 export class App {
   protected readonly title = signal('velo-institutional');
