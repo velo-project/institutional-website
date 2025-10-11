621b0e4039e68d3cbe796bcd16aef301477b2075
jpaveiro <jpaveiros@gmail.com>
2025-10-11 05:16:35 -0300

    refactor: remove unusual import to avoid warnings

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
-import { Navbar } from './core/layout/components/navbar/navbar';
-import { Footer } from './core/layout/components/footer/footer';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar, Footer],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
   providers: [provideAnimations()]
