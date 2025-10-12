cac567bcde2bcc845f12a4ddc04c0b2a9abb1407
jpaveiro <jpaveiros@gmail.com>
2025-10-12 17:43:52 -0300

    feat: tab underline

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../core/layout/components/navbar/navbar';
 import { Footer } from '../../core/layout/components/footer/footer';
+import { TabsUnderlineDirective } from "./tabs-underline.directive";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Footer, Navbar],
+  imports: [Footer, Navbar, TabsUnderlineDirective],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
