5b714d6b2055c3a751890b1990f28bb13c8cde65
jpaveiro <jpaveiros@gmail.com>
2025-10-13 23:04:22 -0300

    feat: add 'sobre' route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { AboutPage } from './features/about-page/about-page';
 
 // TODO TitleStrategy
 export const routes: Routes = [
@@ -7,5 +8,10 @@ export const routes: Routes = [
     title: 'Velo',
     path: '', 
     component: Home
+  },
+  {
+    title: 'Sobre nós',
+    path: 'sobre',
+    component: AboutPage
   }
 ];
