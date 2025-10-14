721bf457944f66404ccbf97b57d26bcb26047089
andreysantoniello <andreynardy@gmail.com>
2025-10-14 00:05:41 -0300

    feat: add SupportPage route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,23 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { AboutPage } from './features/about-page/about-page';
+import { SupportPage } from './features/support-page/support-page';
 
 // TODO TitleStrategy
 export const routes: Routes = [
   {
     title: 'Velo',
-    path: '', 
+    path: '',
     component: Home
   },
   {
     title: 'Sobre nós',
     path: 'sobre',
     component: AboutPage
+  },
+  {
+    title: 'Suporte',
+    path: 'suporte',
+    component: SupportPage
   }
 ];
