b4586f6907c51fcda2e66a81427c33ad5f006b72
andreysantoniello <andreynardy@gmail.com>
2025-10-27 23:56:08 -0300

    feat: refactor AboutPage layout and styles for improved design and responsiveness

--- src/app/features/about-page/about-page.ts
+++ src/app/features/about-page/about-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../../core/layout/components/navbar/navbar";
 import { Footer } from "../../core/layout/components/footer/footer";
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-page',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, ReactiveFormsModule, CommonModule],
   templateUrl: './about-page.html',
   styleUrl: './about-page.scss'
 })
